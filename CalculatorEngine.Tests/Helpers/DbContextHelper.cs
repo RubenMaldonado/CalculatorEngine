8ea4160ae65dfbd6aa29fa8f37ce25ea3409f5f1
Ruben Maldonado <ruben@maldonadotena.com>
2020-02-15 18:26:02 -0600

    add test helepers and dbs

---
+++ CalculatorEngine.Tests/Helpers/DbContextHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using CalculatorEngine.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalculatorEngine.Tests
+{
+    public class DbContextHelper
+    {
+        public async Task<ICalculatorDbContext> GetCalculatorDbContext()
+        {
+
+            // Create an inMemoryDatabase in order to be used by our tests.
+            var options = new DbContextOptionsBuilder<ICalculatorDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var dbContext = new ICalculatorDbContext(options);
+
+            dbContext.Database.EnsureCreated();
+
+            // Initialize Coefficients with incremental values 1,2,3l4
+            if (await dbContext.Coefficients.CountAsync() <= 0)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    dbContext.Coefficients.Add(new Coefficient()
+                    {
+                        Id = Guid.NewGuid(),
+                        CoefficientValue = i
+                    });
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+            return dbContext;
+        }
+    }
+}
---
+++ CalculatorEngine/Data/CalculatorDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CalculatorEngine.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalculatorEngine
+{
+    // Interface for Db Context ------------------------------------------------
+    public interface IDbContext
+    {
+
+        DbSet<Coefficient> Coefficients { get; set; }
+
+    }
+
+    // Db Context --------------------------------------------------------------
+    public class ICalculatorDbContext : DbContext, IDbContext
+    {
+        public ICalculatorDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ICalculatorDbContext()
+        {
+        }
+
+
+        public DbSet<Coefficient> Coefficients { get; set; }
+
+    }
+
+}
